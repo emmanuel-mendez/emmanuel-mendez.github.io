68f65c0b4e3530e504e3d207a22f255f1b14adb7
Emmanuel Mendez <emmanuelmendez.dev@outlook.com>
2025-01-13 00:22:31 -0400

    feat: add seo meta tags, favicon fallbacks and create page service

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { LayoutComponent } from '../../templates/layout/layout.component';
 
 @Component({
@@ -7,4 +8,12 @@ import { LayoutComponent } from '../../templates/layout/layout.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private metaService: Meta) {}
+
+  ngOnInit(): void {
+    this.metaService.addTags([
+      { property: 'og:title', content: 'Home - Emmanuel Mendez' },
+    ]);
+  }
+}
